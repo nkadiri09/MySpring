9fb2cb580f83a024818d0f52b9297a023ece2bca
NarendraReddy <nkadiri09@gmail.com>
2017-06-10 22:52:27 -0400

    initial Commit
    Change-Id: I61d82d3f2d46216392f46e053becc1d45ebf52c0
    Signed-off-by: NarendraReddy <nkadiri09@gmail.com>

---
+++ MySpring/SpringDemo/src/org/naren/kadiri/DrawingApp.java
@@ -0,0 +1,16 @@
+package org.naren.kadiri;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		Triangle triangle = (Triangle) factory.getBean("triangle");
+		triangle.draw();
+		
+		//Triangle nnnnnnnnn
+	}
+}
